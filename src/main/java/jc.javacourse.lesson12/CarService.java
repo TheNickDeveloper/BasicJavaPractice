d82ffd4e4d9da0cebf1b093c5cfe0ecc0cec752c
JC Chang (SW) <jc.chang@gogoro.com>
2020-04-04 19:38:27 +0800

    Finish lesson12 - logger part

---
+++ src/main/java/jc.javacourse.lesson12/CarService.java
@@ -0,0 +1,15 @@
+package jc.javacourse.lesson12;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CarService {
+
+  private final Logger log = LoggerFactory.getLogger(CarService.class);
+
+  public void process(String input) {
+    if (log.isDebugEnabled()) {
+      log.debug("processing car: {}", input);
+    }
+  }
+}
